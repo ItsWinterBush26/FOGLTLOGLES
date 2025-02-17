a72d011da908b780c9e98a36931e9c7a2a6b64a0
thatmidcoder393 <73651803+ThatMG393@users.noreply.github.com>
2025-02-17 12:48:42 +0000

    Testinggg

--- src/egl/egl.cpp
+++ src/egl/egl.cpp
@@ -1,36 +1,40 @@
-#include "egl/egl.h"
+#include <egl/egl.h>
 #include "main.h"
 #include "utils/defines.h"
 #include "utils/log.h"
 
+#include <atomic>
+#include <mutex>
 #include <string>
 
-OVERRIDE(EGLContext,
+std::atomic_bool eglFunctionsInit = ATOMIC_VAR_INIT(false);
+std::once_flag eglInitFlag;
+
+constexpr unsigned int str2int(const char* str, int h = 0) {
+    return !str[h] ? 5381 : (str2int(str, h+1) * 33) ^ str[h];
+}
+
+OVERRIDE(
+    EGLContext,
     eglCreateContext,
-    (EGLDisplay dpy, EGLConfig config, EGLContext share_context, const EGLint *attrib_list)
-) {
+    (EGLDisplay dpy, EGLConfig config, EGLContext share_context, const EGLint *attrib_list)) {
     /* EGLContext ctx = */
     LOGI("This where we init right?");
     return original_eglCreateContext(dpy, config, share_context, attrib_list);
     // return ctx;
 }
 
-WRAP(EGLBoolean,
-    eglDestroyContext,
-    (EGLDisplay dpy, EGLContext ctx),
-    (dpy, ctx)
-)
-
-WRAP(EGLBoolean,
-    eglMakeCurrent,
-    (EGLDisplay dpy, EGLSurface draw, EGLSurface read, EGLContext ctx),
-    (dpy, draw, read, ctx)
-)
-
 OVERRIDE(__eglMustCastToProperFunctionPointerType,
     eglGetProcAddress,
     (const char *procname)
 ) {
     LOGI("What could LWJGL be asking bruh??? procname: %s", procname);
-    return getGlFunctionAddress(std::string(procname));
+    switch (str2int(procname)) {
+        case str2int("eglCreateContext"):
+            LOGI("Returning overridden eglCreateContext");
+            return reinterpret_cast<__eglMustCastToProperFunctionPointerType>(eglCreateContext);
+        default: return original_eglGetProcAddress(procname);
+    }
+
+    // return getGlFunctionAddress(std::string(procname));
 }
