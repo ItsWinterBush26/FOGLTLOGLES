a4ea133adcfd1b9a389d9e6bcd568d066e452c00
thatmidcoder393 <73651803+ThatMG393@users.noreply.github.com>
2025-02-20 22:31:13 +0800

    Update defines.h

--- include/utils/defines.h
+++ include/utils/defines.h
@@ -1,52 +1,56 @@
 #pragma once
 
 #include "api.h"
+#include <dlfcn.h>
 
 #ifndef WRAP
-#define WRAP(type, func, params, args)      \
-using PFN_##func = decltype(&::func);       \
-static PFN_##func original_##func = ::func; \
-PUBLIC_API type func params {               \
-	return original_##func args;            \
+#define WRAP(type, func, params, args)                                  \
+using PFN_##func = decltype(&func);                                       \
+static PFN_##func original_##func = nullptr;                             \
+PUBLIC_API type func params {                                             \
+    if (!original_##func) {                                               \
+        original_##func = (PFN_##func)dlsym(RTLD_NEXT, #func);            \
+    }                                                                     \
+    return original_##func args;                                          \
 }
 #endif
 
 #ifndef WRAPV
-#define WRAPV(func, params, args)           \
-using PFN_##func = decltype(&::func);       \
-static PFN_##func original_##func = ::func; \
-PUBLIC_API void func params {               \
-	original_##func args;                   \
+#define WRAPV(func, params, args)                                         \
+using PFN_##func = decltype(&func);                                       \
+static PFN_##func original_##func = nullptr;                             \
+PUBLIC_API void func params {                                             \
+    if (!original_##func) {                                               \
+        original_##func = (PFN_##func)dlsym(RTLD_NEXT, #func);            \
+    }                                                                     \
+    original_##func args;                                                 \
 }
 #endif
 
 #ifndef REDIRECT
 #define REDIRECT(type, name, target, params, args)  \
 PUBLIC_API type name params {                       \
-	return target args;                           \
+    return target args;                             \
 }
 #endif
 
 #ifndef REDIRECTV
 #define REDIRECTV(name, target, params, args)  \
 PUBLIC_API void name params {                  \
-	target args;                             \
+    target args;                             \
 }
 #endif
 
 #ifndef OVERRIDE
-#define OVERRIDE(type, func, ...)           \
-using PFN_##func = decltype(&::func);       \
-static PFN_##func original_##func = ::func; \
+#define OVERRIDE(type, func, ...)                                       \
+using PFN_##func = decltype(&func);                                     \
+static PFN_##func original_##func = nullptr;                           \
 DEFINE_API(type, func, __VA_ARGS__)
 #endif
 
 #ifndef OVERRIDEV
-#define OVERRIDEV(func, ...)                \
-using PFN_##func = decltype(&::func);       \
-static PFN_##func original_##func = ::func; \
+#define OVERRIDEV(func, ...)                                            \
+using PFN_##func = decltype(&func);                                     \
+static PFN_##func original_##func = nullptr;                           \
 DEFINE_API(void, func, __VA_ARGS__)
 #endif
-
-
-
