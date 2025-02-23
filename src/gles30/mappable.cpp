5b338c4ce2a6b694ba5bd71e677973da9113d14a
AltIsBacc <117834511+AltIsBacc@users.noreply.github.com>
2025-02-23 08:54:53 +0000

    bruh

--- src/gles30/mappable.cpp
+++ src/gles30/mappable.cpp
@@ -1,9 +1,9 @@
-#include "gles30/translation.h"
+#include "gles30/mappable.h"
 #include "main.h"
 
 #include <GLES3/gl3.h>
 
-void GLES30::registerTranslatedFunctions() {
+void GLES30::registerMappableFunctions() {
     REGISTER(glGenVertexArrays);
     REGISTER(glBindVertexArray);
     REGISTER(glUnmapBuffer);
