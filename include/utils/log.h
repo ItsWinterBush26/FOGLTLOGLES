04742316777b23eb030351bfa43e1d5ea93c4b9a
thatmidcoder393 <73651803+ThatMG393@users.noreply.github.com>
2025-02-24 05:13:47 +0800

    use printf logging for now

--- include/utils/log.h
+++ include/utils/log.h
@@ -1,19 +1,27 @@
 #pragma once
 
-#include <android/log.h>
-
 #ifndef TAG
 #define TAG "FOGLTLOGLES"
 #endif
+
+#define PRINTF_LOG
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__)
-#define LOGW(...) __android_log_print(ANDROID_LOG_WARN, TAG, __VA_ARGS__)
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__)
-#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__)
+#ifdef PRINTF_LOG
+    #define LOGE(...) printf(TAG " E: " __VA_ARGS__), printf("\n")
+    #define LOGW(...) printf(TAG " W: " __VA_ARGS__), printf("\n")
+    #define LOGI(...) printf(TAG " I: " __VA_ARGS__), printf("\n")
+    #define LOGD(...) printf(TAG " D: " __VA_ARGS__), printf("\n")
+#else
+    #include <android/log.h>
+    #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__)
+    #define LOGW(...) __android_log_print(ANDROID_LOG_WARN, TAG, __VA_ARGS__)
+    #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__)
+    #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__)
+#endif
 
 #ifdef __cplusplus
 }
