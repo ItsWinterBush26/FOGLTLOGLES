67e1eb7ae5e73d80cce95ce19b195523e4f3fbc9
thatmidcoder393 <73651803+ThatMG393@users.noreply.github.com>
2025-02-23 16:45:47 +0800

    1.17 gl calls

--- src/gles20/mappable.cpp
+++ src/gles20/mappable.cpp
@@ -4,5 +4,36 @@
 #include <GLES2/gl2.h>
 
 void GLES20::registerMappableFunctions() {
-
+    REGISTER(glGetString);
+    REGISTER(glGenVertexArrays);
+    REGISTER(glBindVertexArray);
+    REGISTER(glGenBuffers);
+    REGISTER(glBindBuffer);
+    REGISTER(glBufferData);
+    REGISTER(glMapBuffer);
+    REGISTER(glUnmapBuffer);
+    REGISTER(glVertexAttribPointer);
+    REGISTER(glEnableVertexAttribArray);
+    REGISTER(glCreateShader);
+    REGISTER(glShaderSource);
+    REGISTER(glCompileShader);
+    REGISTER(glGetShaderiv);
+    REGISTER(glCreateProgram);
+    REGISTER(glAttachShader);
+    REGISTER(glLinkProgram);
+    REGISTER(glUseProgram);
+    REGISTER(glGenTextures);
+    REGISTER(glActiveTexture);
+    REGISTER(glBindTexture);
+    REGISTER(glTexImage2D);
+    REGISTER(glTexParameteri);
+    REGISTER(glGenerateMipmap);
+    REGISTER(glDrawArrays);
+    REGISTER(glDrawElements);
+    REGISTER(glClear);
+    REGISTER(glEnable);
+    REGISTER(glDisable);
+    REGISTER(glBlendFunc);
+    REGISTER(glDepthFunc);
+    REGISTER(glViewport);
 }
