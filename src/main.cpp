0739f6fb277f3efbe6978b76e37f317af4b578c7
AltIsBacc <117834511+AltIsBacc@users.noreply.github.com>
2025-03-05 04:53:58 +0000

    export the call

--- src/main.cpp
+++ src/main.cpp
@@ -45,7 +45,7 @@ void FOGLTLOGLES::init() {
 }
 
 extern "C" {
-    jint JNI_OnLoad(JavaVM* vm, void* reserved) {
+    JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
         JNIEnv* env = NULL;
     
         if (vm->GetEnv((void**) &env, JNI_VERSION_1_6) != JNI_OK) {
