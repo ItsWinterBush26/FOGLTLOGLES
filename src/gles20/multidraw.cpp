7c8c9068ac9804235bcedb0588bea1926049f5e1
AltIsBacc <117834511+AltIsBacc@users.noreply.github.com>
2025-03-20 10:49:41 +0000

    scrap multithreading

--- src/gles20/multidraw.cpp
+++ src/gles20/multidraw.cpp
@@ -4,10 +4,6 @@
 
 #include <GLES2/gl2.h>
 
-#ifndef DRAWCOUNT_THRESHOLD
-#define DRAWCOUNT_THRESHOLD 256
-#endif
-
 void glMultiDrawArrays(GLenum mode, const GLint* first, const GLsizei* count, GLsizei drawcount);
 void glMultiDrawElements(GLenum mode, const GLsizei* count, GLenum type, const void* const* indices, GLsizei drawcount);
 
@@ -24,7 +20,6 @@ void glMultiDrawArrays(
 ) {
     if (drawcount == 0) return;
 
-    #pragma omp parallel for if (drawcount > DRAWCOUNT_THRESHOLD)
     for (GLsizei i = 0; i < drawcount; ++i) {
         if (count[i] > 0) glDrawArrays(mode, first[i], count[i]);
     }
@@ -38,9 +33,7 @@ void glMultiDrawElements(
     GLsizei drawcount
 ) {
     if (drawcount == 0) return;
-    // if (!Validation::isCurrentFramebufferValid()) return;
-
-    #pragma omp parallel for if (drawcount > DRAWCOUNT_THRESHOLD)
+    
     for (GLsizei i = 0; i < drawcount; ++i) {
         if (count[i] > 0) glDrawElements(mode, count[i], type, indices[i]);
     }
