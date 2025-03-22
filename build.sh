ddfc945e2825527fc8c774b9dd9ee56398ccacb9
AltIsBacc <117834511+AltIsBacc@users.noreply.github.com>
2025-03-22 00:44:14 +0000

    ugh

--- build.sh
+++ build.sh
@@ -35,7 +35,7 @@ else
     echo "Using NDK at '$ANDROID_NDK_ROOT'"
 fi
 
-git submodule update --init --recursive --depth=1
+git submodule update --remote --merge --init --recursive --depth=1
 ./deps/shaderc/utils/git-sync-deps
 
 rm -rf build
