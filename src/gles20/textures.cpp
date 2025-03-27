4f41e1e391fa0caee59ccc9f0602a4db546c6440
thatmidcoder393 <73651803+ThatMG393@users.noreply.github.com>
2025-03-27 12:28:03 +0800

    Update textures.cpp

--- src/gles20/textures.cpp
+++ src/gles20/textures.cpp
@@ -132,14 +132,17 @@ void OV_glCopyTexSubImage2D(
 
     GLint texture;
     glGetIntegerv(GL_TEXTURE_BINDING_2D, &texture);
-
-    GLenum internalformat = trackedTextures[texture];
-
-    if (isDepthFormat(target)) {
+    
+    if (isDepthFormat(trackedTextures[texture])) {
         LOGI("Bound image is a depth texture, re-routing to FakeDepthFramebuffer");
         fakeDepthbuffer->blitCurrentReadToFakeDraw(target, level, x, y, width, height);
     } else {
         glCopyTexSubImage2D(target, level, xoffset, yoffset, x, y, width, height);
+        if (eglGetError() == GL_INVALID_OPERATION) {
+            LOGI("glCopyTexSubImage2D failed with GL_INVALID_OPERATION. Re-routing to FakeDepthFramebuffer");
+        
+            fakeDepthbuffer->blitCurrentReadToFakeDraw(target, level, x, y, width, height);
+        }
     }
 }
 
