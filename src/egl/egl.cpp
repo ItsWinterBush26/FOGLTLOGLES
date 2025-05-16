b53a85924bf5c7b01bcda3a3f55b5ff065884c64
AltIsBacc <117834511+AltIsBacc@users.noreply.github.com>
2025-05-16 23:15:54 +0000

    hijack eglChooseConfig

--- src/egl/egl.cpp
+++ src/egl/egl.cpp
@@ -6,6 +6,7 @@
 #include <EGL/egl.h>
 #include <dlfcn.h>
 #include <mutex>
+#include <vector>
 
 FunctionPtr eglGetProcAddress(str procname) {
     std::call_once(eglInitFlag, eglInit);
@@ -15,6 +16,42 @@ FunctionPtr eglGetProcAddress(str procname) {
     if (real_eglGetProcAddress) return real_eglGetProcAddress(procname);
     
     return nullptr;
+}
+
+EGLBoolean OV_eglChooseConfig(
+    EGLDisplay dpy,
+    const EGLint* attrib_list,
+    EGLConfig* configs,
+    EGLint config_size,
+    EGLint* num_config
+) {
+    EGLint count = 0;
+    while (attrib_list[count] != EGL_NONE) {
+        count += 2;
+    }
+
+    std::vector<EGLint> attrib_list_mod(attrib_list, attrib_list + count);
+
+    bool foundRenderableType;
+    int index = 0;
+    for (const auto& attrib : attrib_list_mod) {
+        if (attrib == EGL_RENDERABLE_TYPE) {
+            foundRenderableType = true;
+
+            index++;
+            continue;
+        }
+
+        if (foundRenderableType) {
+            if (!(attrib_list_mod[index] & EGL_OPENGL_ES_BIT)) attrib_list_mod[index] = attrib_list_mod[index] | EGL_OPENGL_ES_BIT;
+            break;
+        }
+
+        index++;
+    }
+    attrib_list_mod.push_back(EGL_NONE);
+
+    return eglChooseConfig(dpy, attrib_list_mod.data(), configs, config_size, num_config);
 }
 
 EGLBoolean OV_eglMakeCurrent(EGLDisplay dpy, EGLSurface draw, EGLSurface read, EGLContext ctx) {
@@ -27,7 +64,7 @@ inline void eglInit() {
     LOGI("Initializing EGL functions...");
 
     REGISTER(eglBindAPI);
-    REGISTER(eglChooseConfig);
+    REGISTEROV(eglChooseConfig);
     REGISTER(eglCreateContext);
     REGISTER(eglCreatePbufferSurface);
     REGISTER(eglCreateWindowSurface);
