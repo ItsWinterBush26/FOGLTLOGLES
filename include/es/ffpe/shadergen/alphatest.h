66e9637e517c37ef8bb8af43dcd2a99e53b46c4b
ThatMG393 <thatmg393@gmail.com>
2025-05-26 00:28:36 +0000

    bro how???

--- include/es/ffpe/shadergen/alphatest.h
+++ include/es/ffpe/shadergen/alphatest.h
@@ -58,7 +58,7 @@ void build(
         operation
     );
 
-    finalOperations += FFPE::Rendering::ShaderGen::SG_NEWLINE;
+    finalOperations += "\n\n"; // FFPE::Rendering::ShaderGen::SG_NEWLINE;
 }
 
 void sendData(GLuint program) override {
