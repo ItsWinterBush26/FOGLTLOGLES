ddbd12a793001be8cdb97d96ac2795f04361de3e
thatmidcoder393 <73651803+ThatMG393@users.noreply.github.com>
2025-05-29 11:05:43 +0800

    postpone compute support

--- src/gles20/ext/arb_compute_shader.cpp
+++ src/gles20/ext/arb_compute_shader.cpp
@@ -3,7 +3,7 @@
 #include "utils/log.h"
 
 void GLES20Ext::register_GL_ARB_compute_shader() {
-    LOGI("Exposing GL_ARB_compute_shader.");
+    LOGI("Not exposing GL_ARB_compute_shader.");
 
-    ESUtils::fakeExtensions.insert("GL_ARB_compute_shader");
+    // ESUtils::fakeExtensions.insert("GL_ARB_compute_shader");
 }
